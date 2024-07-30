1c7ff25592a37af2b8c96e01a9bd61c4cfe51f74
Foad Alhayek <foad_97@hotmail.com>
2024-07-30 17:36:55 +0200

    Installed i18n to the project, testing against Nian

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from "path"
+import { resolve } from "path";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
@@ -12,4 +12,19 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      { code: "en", iso: "en-US", name: "English", files: ["en/nian.json"] },
+      { code: "sv", iso: "sv-SE", name: "Svenska", files: ["sv/nian.json"] },
+    ],
+    lazy: true,
+    langDir: "locales",
+    defaultLocale: "en",
+    strategy: "no_prefix",
+    vueI18n: "i18n.config.ts",
+  },
+  experimental: {
+    scanPageMeta: true,
+  },
+});
